a62be6a13ca5172830fa7e6d653ed3d031f4c9e6
kzq1992 <kzq1992@gmail.com>
2018-07-28 09:55:16 +0800

    优化MP4toYUV

--- app/src/main/java/com/kzq/study/Mp4toYUVActivity.java
+++ app/src/main/java/com/kzq/study/Mp4toYUVActivity.java
@@ -3,6 +3,7 @@ package com.kzq.study;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,11 +26,14 @@ public class Mp4toYUVActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 convertBtn.setEnabled(false);
+                final String parent = Environment.getExternalStorageDirectory().getAbsolutePath().trim() + "/";
+                final String file = fileEdt.getText().toString().trim();
+
+                Log.d("kzq", parent + file);
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-
-                        JniTest.mp4toyuv(Environment.getExternalStorageDirectory().getAbsolutePath(), fileEdt.getText().toString());
+                        JniTest.mp4toyuv(parent, file);
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
